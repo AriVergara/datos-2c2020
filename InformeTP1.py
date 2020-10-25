25f178c534364e7538c06e933ac474e2a8f0d940
avergara <avergara@eryx.co>
2020-10-25 19:49:09 -0300

    Se agregan los requirements y se modifican algunos gráficos en conjunto

--- InformeTP1.py
+++ InformeTP1.py
@@ -21,18 +21,18 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import seaborn as sns
 from matplotlib import pyplot as plt
 from pandas_profiling import ProfileReport
-import seaborn as sns
+from sklearn.metrics import accuracy_score
 
 # %%
-import textwrap
-def formatear_titulo(texto, longitud):
-    return "\n".join(textwrap.wrap(texto, longitud))
+sns.set()
 
 
 # %%
-def ajustar_leyenda(axes, labels_mapeados):
+def ajustar_leyenda(axes, labels_mapeados, label_leyenda):
+    axes.legend(title=label_leyenda)
     for t in axes.get_legend().texts:
         t.set_text(labels_mapeados[t.get_text()])
         
@@ -42,7 +42,7 @@ def ajustar_leyenda_columna_volveria(axes, loc=None):
         '0': "No",
         '1': "Sí"
     }
-    ajustar_leyenda(axes, labels_mapeados)
+    ajustar_leyenda(axes, labels_mapeados, "Volvería")
 
 
 # %% [markdown]
@@ -70,148 +70,238 @@ df_datos
 df = df_volvera.merge(df_datos, how='inner', right_on='id_usuario', left_on='id_usuario')
 
 # %%
-df.head()
+df.head(10)
 
 # %%
-#from pandas_profiling import ProfileReport
+from pandas_profiling import ProfileReport
 
-#report = ProfileReport(
-    #df, title='Resumen de datos TP Parte 1', explorative=True, lazy=False
-#)
+report = ProfileReport(
+    df, title='Resumen de datos TP Parte 1', explorative=True, lazy=False
+)
 
 # %%
 #report.to_file('reporte.html')
+report.to_widgets()
 
 # %% [markdown]
-# Se generó un reporte de Pandas Profiling (reporte.html) para tener una visión general del dataset. Se observaron algunas cuestiones interesantes:
+# Se generó un reporte de Pandas Profiling para tener una visión general del dataset. Se observaron algunas cuestiones interesantes:
 #
-# - la clase mayoritaria en el dataset es la que corresponde a las personas que no volverían a ver Frozen 4 (61,6% de los encuestados).
-# - las únicas columnas que presentan missing values son `fila` y `edad`, con un 20% y 78% de valores nulos respectivamente.
+# - La clase mayoritaria en el dataset es la que corresponde a las personas que no volverían a ver Frozen 4 (61,6% de los encuestados).
+# - Las únicas columnas que presentan missing values son `nombre_sede`, `fila` y `edad`, con un 0.25% (solo dos entradas del dataset), 20% y 77.9% de valores nulos respectivamente.
+# - Existen dos variables categóricas ordinales, que son las columnas `amigos` y `parientes`.
+# - Hay mayor cantidad de encuestados hombres que mujeres.
+# - La mayor cantidad de las encuestas corresponden a la sede de Palermo.
+# - La sala 4d es la más elegida por los encuestados.
+# - La mayoría de los encuestados fue a ver Frozen 3 sin ningún pariente y/o amigo.
 #
 # Dado que la columna `fila` presenta una gran cantidad de missing values, se optó por eliminarla del dataframe.
-
-# %%
-print(f'Porcentaje de valores nulos para la columna "fila": {df.fila.isnull().sum()/len(df) * 100}')
 
 # %%
 df.drop(axis=1, columns=["fila"], inplace=True)
 
 # %% [markdown]
-# A continuación se analizan las distintas columnas presentes en el dataset.
-
-# %% [markdown]
-# ### Columna `id_ticket`
+# A su vez, todos los valores de la columna `nombre` son únicos, y al ser el nombre del encuestado no aporta información respecto a su decisión sobre volver a ver Frozen 4. Por ello, se decidió eliminar la columna del dataset para el análisis.
 
 # %%
-df.head()
+df.drop(axis=1, columns=["nombre"], inplace=True)
 
 # %% [markdown]
-# Se puede ver que columna `id_ticket` es un string. Una cuestión interesante es ver si existen entradas repetidas para esta columna.
-
-# %%
-df_tickets_repetidos = df[df.duplicated(subset=["id_ticket"])]
-print(f'Cantidad de filas con id_ticket repetido: {len(df_tickets_repetidos)}')
-df_tickets_repetidos.head()
-
-# %% [markdown]
-# Al analizar los valores se puede obsevar que, si bien el `id_ticket` aparece repetido, el nombre de la persona que completó la encuesta es diferente, así como su `id_usuario`. Se procede a verificar que esto ocurre para todas las entradas.
-
-# %%
-print(f'Cantidad de filas con id_ticket y id_usuario repetido: {len(df[df.duplicated(subset=["id_ticket", "id_usuario"])])}')
-print(f'Cantidad de filas con id_ticket y id_usuario repetido: {len(df[df.duplicated(subset=["id_ticket", "nombre"])])}')
-
-# %% [markdown]
-# A partir de esto, se decidió eliminar la columna del dataset, dado que no parece aportar información para determinar si una persona va volver al cine.
+# También se observó que la columna `id_ticket` presenta entradas repetidas, pero considerando que los valores de `id_usuario` y `nombre` son únicos, estas repeticiones no aportan información para la decisión (el que más se repite lo hace 7 veces). Por ello se decidió eliminar la columna.
 
 # %%
 df.drop(axis=1, columns=["id_ticket"], inplace=True)
 
 # %% [markdown]
+# A continuación se analizan las demás columnas presentes en el dataset en relación con la columna objetivo.
+
+# %% [markdown]
 # ### Columnas `tipo_de_sala` y `nombre_sede`
 
 # %% [markdown]
-# Primero, se analiza la cantidad de encuestados considerando el tipo de sala y si volvería o no.
+# Primero, se analiza la cantidad de encuestados considerando el tipo de sala y si volvería o no a ver Frozen 4.
 
 # %%
+salas = df.tipo_de_sala.unique()
+
 fig, axes = plt.subplots(nrows=1, ncols=2, dpi=100, figsize=(6.4 * 2, 4.8))
-sns.countplot(x="tipo_de_sala", hue="volveria", data=df, ax=axes[0])
+fig.suptitle("Análisis de encuestados según el tipo de sala y si volvería a ver Frozen 4")
+
+sns.countplot(x="tipo_de_sala", hue="volveria", data=df, ax=axes[0], order=salas)
 axes[0].set_ylabel("Cantidad")
 axes[0].set_xlabel("Tipo de sala")
-axes[0].set_title("Cantidad de encuestados segun tipo de sala y si volvería a ver Frozen 4")
+axes[0].set_title("Cantidad para cada sala")
 ajustar_leyenda_columna_volveria(axes[0])
 
 
-sns.countplot(x="tipo_de_sala", data=df, ax=axes[1])
-axes[1].set_ylabel("Cantidad")
+sns.barplot(
+    data=df.groupby("tipo_de_sala")
+    .volveria.value_counts(normalize=True)
+    .rename("volveria_prop")
+    .mul(100)
+    .reset_index(),
+    x='tipo_de_sala',
+    y="volveria_prop",
+    hue='volveria',
+    ax=axes[1],
+    order=salas
+)
+axes[1].set_ylabel("Porcentaje (%)")
 axes[1].set_xlabel("Tipo de sala")
-axes[1].set_title("Cantidad de encuestados segun tipo de sala")
+axes[1].set_title("Porcentaje para cada sala")
+ajustar_leyenda_columna_volveria(axes[1])
+
 plt.show()
 
 # %%
-df[df.tipo_de_sala == '4d'].volveria.value_counts()
+print("Cantidad de encuestados por tipo de sala")
+display(df.tipo_de_sala.value_counts())
+print(" ")
+print("Porcentaje de encuestados por tipo de sala")
+display(df.tipo_de_sala.value_counts().div(df.pipe(len)).mul(100))
 
 # %%
-df.tipo_de_sala.value_counts()
+cuatro_d_value_counts = df[df.tipo_de_sala == '4d'].volveria.value_counts()
+cantidad_de_encuestados = len(df)
+print(f'Porcentaje de encuestados que fueron a 4d y no volverían : { cuatro_d_value_counts[0] / (cuatro_d_value_counts[0] + cuatro_d_value_counts[1]) * 100}')
 
 # %% [markdown]
 #
-# Se observa que el único tipo de sala que presenta una diferencia notoria es la de 4d (el 76% de los que van a esta sala optan por no volver), que a su vez es la que más entradas presenta en el dataset (aproximadamente un 55%). Considerando esto, que el tipo de sala sea 4d podría servir en el baseline para determinar si una persona vuelve.
+# Se observa que el único tipo de sala que presenta una diferencia notoria es la de 4d (el 76% de los que van a esta sala optan por no volver), que a su vez es la que más entradas presenta en el dataset (aproximadamente un 55%, como se vió en el reporte). Considerando esto, que el tipo de sala sea 4d podría servir en el baseline para determinar si una persona vuelve.
 #
 # Se repite el análisis pero para la columna `nombre_sede`.
 
 # %%
+sedes = df.nombre_sede.dropna().unique()
+
 fig, axes = plt.subplots(nrows=1, ncols=2, dpi=100, figsize=(6.4 * 2, 4.8))
-sns.countplot(x="nombre_sede", hue="volveria", data=df, ax=axes[0])
+fig.suptitle("Análisis de encuestados según la sede y si volvería a ver Frozen 4")
+
+sns.countplot(x="nombre_sede", hue="volveria", data=df, ax=axes[0], order=sedes)
 axes[0].set_ylabel("Cantidad")
 axes[0].set_xlabel("Sede")
-axes[0].set_title("Cantidad de encuestados segun sede del cine y si volvería a ver Frozen 4")
+axes[0].set_title("Cantidad para cada sede")
 ajustar_leyenda_columna_volveria(axes[0])
 
-
-sns.countplot(x="nombre_sede", data=df, ax=axes[1])
-axes[1].set_ylabel("Cantidad")
+sns.barplot(
+    data=df.groupby("nombre_sede")
+    .volveria.value_counts(normalize=True)
+    .rename("volveria_prop")
+    .mul(100)
+    .reset_index(),
+    x='nombre_sede',
+    y="volveria_prop",
+    hue='volveria',
+    ax=axes[1],
+    order=sedes
+)
+axes[1].set_ylabel("Porcentaje (%)")
 axes[1].set_xlabel("Sede")
-axes[1].set_title("Cantidad de encuestados segun sede del cine")
+axes[1].set_title("Porcentaje para cada sede")
+ajustar_leyenda_columna_volveria(axes[1])
+
 plt.show()
 
 # %%
-df.nombre_sede.value_counts()
+print("Cantidad de encuestados por sede")
+display(df.nombre_sede.value_counts())
+print(" ")
+print("Porcentaje de encuestados por sede")
+display(df.nombre_sede.value_counts().div(df.pipe(len)).mul(100))
 
 # %%
-df[df.nombre_sede == "fiumark_palermo"].volveria.value_counts()
+cuatro_d_value_counts = df[df.nombre_sede == 'fiumark_palermo'].volveria.value_counts()
+cantidad_de_encuestados = len(df)
+print(f'Porcentaje de encuestados que fueron a Palermo y no volverían : { cuatro_d_value_counts[0] / (cuatro_d_value_counts[0] + cuatro_d_value_counts[1]) * 100}')
 
 # %% [markdown]
-# Se observa que la gran mayoría de las encuestas corresponden a la sede de Palermo (aproximadamente un 73%) y esta es la única donde se observa una diferencia notoria entre ambas clases. Esta se debe seguramente a que hay más encuestados de una clase que de otra, como se vio al comienzo del análisis, dado que la proporción es similar (66% de las personas no volverían), por lo que pareciera ser que no es algo que se pueda asociar a la sede de Palermo.
+# Como se vió en el reporte, la mayoría de las encuestas correspondern a Palermo (aproximadamente un 73%) y esta es la única sede donde se observa una diferencia notoria entre ambas clases. Esta se debe seguramente a que hay más encuestados de una clase que de otra, como se vio al comienzo del análisis, dado que la proporción es similar (66% de las personas no volverían), por lo que pareciera ser que no es algo que se pueda asociar a la sede de Palermo.
 #
 # Se optó por ver si existía alguna diferencia entre los tipos de sala para cada una de las sedes:
 
 # %%
-fig, axes = plt.subplots(nrows=1, ncols=3, dpi=100, figsize=(6.4 * 2, 4.8), sharey=True)
+fig, axes = plt.subplots(nrows=2, ncols=3, dpi=100, figsize=(6.4 * 3, 4.8 * 2 + 2), sharey='row')
+salas = df.tipo_de_sala.unique()
+
+#Palermo
 palermo = df[df["nombre_sede"] == "fiumark_palermo"] 
-sns.countplot(x="tipo_de_sala", hue="volveria", data=palermo, ax=axes[0])
-axes[0].set_ylabel("Cantidad")
-axes[0].set_xlabel("Sede")
-axes[0].set_title("Palermo")
-ajustar_leyenda_columna_volveria(axes[0])
+sns.countplot(x="tipo_de_sala", hue="volveria", data=palermo, ax=axes[0][0], order=salas)
+axes[0][0].set_ylabel("Cantidad")
+axes[0][0].set_xlabel("Tipo de sala")
+axes[0][0].set_title("Palermo - Cantidad para cada sala")
+ajustar_leyenda_columna_volveria(axes[0][0])
+
+sns.barplot(
+    data=palermo.groupby("tipo_de_sala")
+    .volveria.value_counts(normalize=True)
+    .rename("volveria_prop")
+    .mul(100)
+    .reset_index(),
+    x='tipo_de_sala',
+    y="volveria_prop",
+    hue='volveria',
+    ax=axes[1][0],
+    order=salas
+)
+axes[1][0].set_ylabel("Porcentaje (%)")
+axes[1][0].set_xlabel("Tipo de sala")
+axes[1][0].set_title("Palermo - Porcentaje para cada sala")
+ajustar_leyenda_columna_volveria(axes[1][0])
 
 
+#Chacarita
 chacarita = df[df["nombre_sede"] == "fiumark_chacarita"] 
-sns.countplot(x="tipo_de_sala", hue="volveria", data=chacarita, ax=axes[1])
-axes[1].set_ylabel("Cantidad")
-axes[1].set_xlabel("Sede")
-axes[1].set_title("Chacarita")
-ajustar_leyenda_columna_volveria(axes[1])
+sns.countplot(x="tipo_de_sala", hue="volveria", data=chacarita, ax=axes[0][1], order=salas)
+axes[0][1].set_ylabel("Cantidad")
+axes[0][1].set_xlabel("Tipo de sala")
+axes[0][1].set_title("Chacarita - Cantidad para cada sala")
+ajustar_leyenda_columna_volveria(axes[0][1])
 
+sns.barplot(
+    data=chacarita.groupby("tipo_de_sala")
+    .volveria.value_counts(normalize=True)
+    .rename("volveria_prop")
+    .mul(100)
+    .reset_index(),
+    x='tipo_de_sala',
+    y="volveria_prop",
+    hue='volveria',
+    ax=axes[1][1],
+    order=salas
+)
+axes[1][1].set_ylabel("Porcentaje (%)")
+axes[1][1].set_xlabel("Tipo de sala")
+axes[1][1].set_title("Chacarita - Porcentaje para cada sala")
+ajustar_leyenda_columna_volveria(axes[1][1])
+
+
+#Quilmes
 quilmes = df[df["nombre_sede"] == "fiumark_quilmes"] 
-sns.countplot(x="tipo_de_sala", hue="volveria", data=quilmes, ax=axes[2])
-axes[2].set_ylabel("Cantidad")
-axes[2].set_xlabel("Sede")
-axes[2].set_title("Quilmes")
-ajustar_leyenda_columna_volveria(axes[2])
+sns.countplot(x="tipo_de_sala", hue="volveria", data=quilmes, ax=axes[0][2], order=salas)
+axes[0][2].set_ylabel("Cantidad")
+axes[0][2].set_xlabel("Tipo de sala")
+axes[0][2].set_title("Quilmes - Cantidad para cada sala")
+ajustar_leyenda_columna_volveria(axes[0][2])
+
+sns.barplot(
+    data=quilmes.groupby("tipo_de_sala")
+    .volveria.value_counts(normalize=True)
+    .rename("volveria_prop")
+    .mul(100)
+    .reset_index(),
+    x='tipo_de_sala',
+    y="volveria_prop",
+    hue='volveria',
+    ax=axes[1][2],
+    order=salas
+)
+axes[1][2].set_ylabel("Porcentaje (%)")
+axes[1][2].set_xlabel("Tipo de sala")
+axes[1][2].set_title("Quilmes - Porcentaje para cada sala")
+ajustar_leyenda_columna_volveria(axes[1][2])
 
 
 # %% [markdown]
-# Este gráfico no aporto ningún insight interesante.
+# En la sede de Quilmes, practicamente todos los encuestados corresponde a personas que fueron a salas 4d. Este gráfico no aportó ningún insight interesante, dado que ya se había podido oberver que en las salas 3d las proporciones entre las clases son similares mientras que en las normales hay mayor tendencia a querer volver a ver Frozen 4.
 
 # %% [markdown]
 # ### Columna `genero`
@@ -275,6 +365,7 @@ sns.boxplot(
     x="volveria",
 )
 plt.ylabel("Edad")
+plt.xlabel("Volvería")
 plt.xticks([False, True], ["No", "Sí"])
 plt.show()
 
@@ -447,7 +538,7 @@ plt.show()
 
 # %%
 plt.figure(dpi=100)
-plt.title("Distribución de la edad de los encuestados de acuerdo a su genero")
+plt.title("Distribución de la edad de los encuestados de acuerdo a su genero \n y si volvería o no")
 sns.boxplot(
     data=df,
     y="edad",
@@ -457,7 +548,7 @@ sns.boxplot(
 plt.ylabel("Edad")
 plt.xlabel("Género")
 plt.legend(loc="upper center")
-ajustar_leyenda(plt.gcf().axes[0], {'0': 'No volveria', '1': 'Volveria'})
+ajustar_leyenda_columna_volveria(plt.gcf().axes[0])
 plt.show()
 
 # %% [markdown]
@@ -485,7 +576,7 @@ axes[1].set_ylabel("Cantidad")
 axes[1].set_xlabel("Acompañantes")
 axes[1].set_title('Mujeres')
 ajustar_leyenda_columna_volveria(axes[1])
-figs.suptitle("Cantidad de encuestados segun cantidad de acompañantes y si volveria a ver Frozen 4")
+figs.suptitle("Cantidad de encuestados segun cantidad de acompañantes y si volvería a ver Frozen 4")
 plt.show()
 
 # %%
@@ -500,17 +591,18 @@ print(f'Porcentaje de hombres que fueron con dos acompañantes y declararon que 
 
 # %% [markdown]
 # Pueden extraerse dos observaciones del resultado de este analisis:
-# - En el genero masculino, la mayor diferencia entre respuestas positivas y negativas sobre si volverian a ver Frozen 4 se encuentra cuando declaran haber ido sin acompañantes. Luego, al aumentar el numero de acompañantes, se reduce la diferencia.
-# - En el genero femenino el comportamiento es similar, con la diferencia que el mayor porcentaje es de respuestas positivas.
+# - En el género masculino, la mayor diferencia entre respuestas positivas y negativas sobre si volverían a ver Frozen 4 se encuentra cuando declaran haber ido sin acompañantes. Luego, al aumentar el numero de acompañantes, se reduce la diferencia.
+# - En el género femenino el comportamiento es similar, con la diferencia que el mayor porcentaje es de respuestas positivas.
 
 # %% [markdown]
-# Recordando lo visto en [el analisis de los acompañantes](#Columnas-parientes-y-amigos) se puede adjudicar este comportamiento a la diferencia en cantidad de encuestas respecto del numero de acompañantes.
+# Recordando lo visto en [el analisis de los acompañantes](#Columnas-parientes-y-amigos) se puede adjudicar este comportamiento a la diferencia en cantidad de encuestas respecto del número de acompañantes.
+
 
 # %% [markdown]
 # ### Relacionando Genero y Precio del ticket
 
 # %% [markdown]
-# En busqueda de una relación con mayor fuerza entre el genero y otros factores, se buscó relacionarlo con el precio pagado por la entrada:
+# En busqueda de una relación con mayor fuerza entre el genero y otros factores, se intentó relacionarlo con el precio pagado por la entrada:
 
 # %%
 figs, axes = plt.subplots(ncols=2, nrows=1, sharey=True, dpi=100)
@@ -677,10 +769,10 @@ plt.show()
 
 
 # %% [markdown]
-# ## Planteo del baseline
+# ## Armado del baseline
 
 # %%
-def baseline(fila):
+def clasificar_encuestado(fila):
     if fila['edad'] < 20 and fila['parientes'] + fila['amigos'] <= 3:
         return 1
     if fila['genero'] == 'hombre':
@@ -693,30 +785,15 @@ def baseline(fila):
 
 
 # %%
-df['volveria_1'] = 0
-
-# %%
-df.head()
-
-# %%
-
-for nro_fila in range(len(df)):
-    df.loc[nro_fila,'volveria_1'] = baseline(df.loc[nro_fila,:])
+def baseline(X):
+    resultado = []
+    for nro_fila in range(len(X)):
+        resultado.append(clasificar_encuestado(df.loc[nro_fila,:]))
+    return resultado
 
 
 # %%
-df.head()
+prediccion = baseline(df)
 
 # %%
-df_aciertos = df[df.volveria == df.volveria_1]
-
-# %%
-len(df_aciertos[df_aciertos.genero == 'mujer']) / len(df[df.genero == 'mujer'])
-
-# %%
-len(df_aciertos[df_aciertos.genero == 'hombre']) / len(df[df.genero == 'hombre'])
-
-# %%
-len(df_aciertos) / len(df)
-
-# %%
+accuracy_score(df.volveria, prediccion)
