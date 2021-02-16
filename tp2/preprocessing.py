6fb0ea40176449e28150ba9018660ca1bf430658
Ariel <vergara.ari@gmail.com>
2021-02-16 19:18:50 -0300

    Se agrega el refactor en precoeso de los transformers

--- tp2/preprocessing.py
+++ tp2/preprocessing.py
@@ -18,21 +18,6 @@ RANDOM_STATE = 117
 
 TEST_SIZE = 0.2
 
-class EliminarFilasSinInformacionTransformer(BaseEstimator, TransformerMixin):
-    """
-        Elimina columnas sin infromación valiosa (fila, id_usuario, id_ticket).
-    """
-    def fit(self, X, y=None):
-        return self
-    
-    def transform(self, X):
-        X = X.copy()
-        X = X.drop(columns=["id_usuario"], axis=1, inplace=False)
-        X = X.drop(columns=["nombre"], axis=1, inplace=False)
-        X = X.drop(columns=["id_ticket"], axis=1, inplace=False)
-        return X
-    
-
 
 class PreprocessingLE(BaseEstimator, TransformerMixin):
     """
@@ -338,10 +323,10 @@ class PreprocessingXGBoost2(BaseEstimator, TransformerMixin):
 
 class PreprocessingCategoricalNB1(BaseEstimator, TransformerMixin):
     """
-        -Elimina columnas sin infromación valiosa (fila, id_usuario, id_ticket) y con valores 
+        -Elimina columnas sin información valiosa (fila, id_usuario, id_ticket) y con valores 
             continuos o discretos(parientes, amigos, edad y precio_ticket).
         -Encodea variables categóricas mediante LabelEncoding (genero, nombre_sala, tipo_de_sala)
-        -Agrega columnas edad_isna, fila_isna, va_con_amigos, va_con_parientes
+        -Agrega columnas edad_isna, fila_isna
     """
     def __init__(self):
         super().__init__()
@@ -359,6 +344,9 @@ class PreprocessingCategoricalNB1(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         X = X.copy()
+        X["edad_isna"] = X["edad"].isna().astype(int)
+        X["fila_isna"] = X["fila"].isna().astype(int)
+        
         X['tipo_de_sala_encoded'] = self.le_tipo_sala.transform(X['tipo_de_sala'].astype(str))
         X = X.drop(columns=["tipo_de_sala"], axis=1, inplace=False)
         
@@ -490,63 +478,88 @@ class PreprocessingGaussianNB1(BaseEstimator, TransformerMixin):
         
         return X
 
-# -------------- FUNCIONES AUXILIARES -----------------
-
-def borrar_columna(df, columna, ip=False):
-    if ip:
-        df.drop(axis=1, columns=[columna], inplace=ip)
-    else:
-        return df.drop(axis=1, columns=[columna])
-
-def one_hot_encoding(df, variable, dummy_na=True, drop_first=True):
-    return pd.get_dummies(df, columns=[variable], dummy_na=dummy_na, drop_first=drop_first)    
-
-def label_encoding(df, variable):
-    le = LabelEncoder()
-    return le.fit_transform(df[variable].astype(str))
-
-def redondear_edades(df, columna_edad):
-    df[columna_edad] = df[columna_edad].round().astype(int)
-    return df
+# -------------- TRANSFORMERS AUXILIARES -----------------
+# Refactor para poder hacer que cada uno de los Preprocessor sea una función que devuelve un Pipeline 
+#con los preprocesamientos adecuados
 
 
-def hashing_encoding(df, cols, data_percent=0.85, verbose=False):
-    for i in cols:
-        val_counts = df[i].value_counts(dropna=False)
-        s = sum(val_counts.values)
-        h = val_counts.values / s
-        c_sum = np.cumsum(h)
-        c_sum = pd.Series(c_sum)
-        n = c_sum[c_sum > data_percent].index[0]
-        if verbose:
-            print("n hashing para ", i, ":", n)
-        if n > 0:
-            fh = FeatureHasher(n_features=n, input_type='string')
-            hashed_features = fh.fit_transform(
-                df[i].astype(str).values.reshape(-1, 1)
-            ).todense()
-            df = df.join(pd.DataFrame(hashed_features).add_prefix(i + '_'))
+class EliminarFilasSinInformacionTransformer(BaseEstimator, TransformerMixin):
+    """
+        Elimina columnas sin infromación valiosa (fila, id_usuario, id_ticket).
+    """
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        X = X.copy()
+        X = X.drop(columns=["id_usuario"], axis=1, inplace=False)
+        X = X.drop(columns=["nombre"], axis=1, inplace=False)
+        X = X.drop(columns=["id_ticket"], axis=1, inplace=False)
+        return X
+    
+class LabelEncoderCategoricas(BaseEstimator, TransformerMixin):
+    """
+        - Aplica label encoder sobre las variables categóricas (nombre_sede, genero, tipo_de_sala)
+    """
+    def __init__(self):
+        super().__init__()
+        self.le_tipo_sala = LabelEncoder()
+        self.le_nombre_sede = LabelEncoder()
+        self.le_genero = LabelEncoder()
+        self.moda_nombre_sede = ""
+    
+    def fit(self, X, y=None):
+        self.moda_nombre_sede = X["nombre_sede"].astype(str).mode()[0]
+        self.le_tipo_sala.fit(X['tipo_de_sala'].astype(str))
+        self.le_nombre_sede.fit(X['nombre_sede'].fillna(self.moda_nombre_sede).astype(str))
+        self.le_genero.fit(X['genero'].astype(str))
+        return self
 
-    return df.drop(columns=cols)
+    def transform(self, X):
+        X = X.copy()nplace=False)
+        
+        X["nombre_sede_isna"] = X["nombre_sede"].isna().astype(int)
+        X['nombre_sede'] = X['nombre_sede'].fillna(self.moda_nombre_sede)
+        X['nombre_sede'] = self.le_nombre_sede.transform(X['nombre_sede'].astype(str))
+        
+        X['tipo_de_sala'] = self.le_tipo_sala.transform(X['tipo_de_sala'].astype(str))
+        
+        X['genero'] = self.le_genero.transform(X['genero'].astype(str))
+        
+        return X
+    
+class EdadTransformer(BaseEstimator, TransformerMixin):
+    """
+        - Completa los missing values de la columna edad con la media
+        - Convierte en bins los valores de la columna edad.
+        
+    """
+    def __init__(self, drop_column=False):
+        super().__init__()
+        self.mean_edad = 0
+        self.drop_column = drop_column
+    
+    def fit(self, X, y=None):
+        self.mean_edad = X["edad"].mean()
+        return self
 
+    def transform(self, X):
+        X = X.copy()
 
-def knn_imputer(df):
-
-    cat_cols = ['tipo_de_sala', 'genero', 'nombre_sede']
-
-    # Aplicamos hashing para las categoricas
-    df = hashing_encoding(df, cat_cols)
-
-    # definimos un n arbitrario
-    imputer = KNNImputer(n_neighbors=2, weights="uniform")
-    df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
-    return df.add_suffix('_knn')['edad_knn']
-
-def kbins_discretizer(df, col, n_bins=4):
-    enc = KBinsDiscretizer(n_bins, encode='ordinal')
-
-    _df = df[[col]].dropna().reset_index(drop=True)
-    X_binned = enc.fit_transform(_df)
-    return X_binned.astype(int)
-
-
+        X["edad_isna"] = X["edad"].isna().astype(int)
+        X["edad"] = X["edad"].fillna(self.mean_edad)
+        X["edad_bins"] = X["edad"].apply(self._bins_segun_edad)
+        if self.drop_column:
+            X = X.drop(columns=["edad"], axis=1, inplace=False)
+        return X
+    
+    def _bins_segun_edad(self, edad): 
+        if edad <= 18:
+            return 1
+        if 18 < edad <= 30:
+            return 2
+        if 30 < edad <= 40:
+            return 3
+        if 40 < edad <= 70:
+            return 4
+        return 5
